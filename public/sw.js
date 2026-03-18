654acb5401268e75d629a0b6291704d89b13beb7
Sevih <collin.seb95@gmail.com>
2026-03-18 14:07:23 +0100

    proccess de verif avant release V2

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Version injected by scripts/set-version.js at build time
-const CACHE_NAME = 'outerpedia-cache-v0.1.25';
+const CACHE_NAME = 'outerpedia-cache-v0.2.0';
 
 // Static assets to pre-cache on install
 const PRECACHE_URLS = [
