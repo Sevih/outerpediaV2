766afa88a075312cfa2c633a1b6a8eef693b8553
Sevih <collin.seb95@gmail.com>
2026-03-19 11:38:53 +0100

    sw toast [no-discord]

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Version injected by scripts/set-version.js at build time
-const CACHE_NAME = 'outerpedia-cache-v2.2.1';
+const CACHE_NAME = 'outerpedia-cache-v2.2.4';
 
 // Static assets to pre-cache on install
 const PRECACHE_URLS = [
