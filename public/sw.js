aa6cf5af27ec61d2d00cc18addeac7a99981c971
Sevih <collin.seb95@gmail.com>
2026-03-24 09:49:03 +0100

    cf notia [no-discord]

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Version injected by scripts/set-version.js at build time
-const CACHE_NAME = 'outerpedia-cache-v2.3.12';
+const CACHE_NAME = 'outerpedia-cache-v3.0.0';
 const APP_VERSION = CACHE_NAME.replace('outerpedia-cache-v', '');
 
 // Static assets to pre-cache on install
